debbc91fad843f0d67ce183a01e5483ad484e4fa
yoichi-aoyama <aoyama.yoichi@gmail.com>
2023-12-22 00:23:02 +0900

    mediaType

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -12,79 +11,92 @@ import (
 	"github.com/tdewolff/minify/v2/css"
 	"github.com/tdewolff/minify/v2/html"
 	"github.com/tdewolff/minify/v2/js"
+	"github.com/tdewolff/minify/v2/json"
+	"github.com/tdewolff/minify/v2/svg"
+	"github.com/tdewolff/minify/v2/xml"
 )
 
 func main() {
+	fmt.Println("- minify start -")
+	inputPath := "./src"
+	outputPath := "./dist"
+
+	// setup minify
 	m := minify.New()
 	m.AddFunc("text/css", css.Minify)
 	m.AddFunc("text/html", html.Minify)
-	// m.AddFunc("image/svg+xml", svg.Minify)
+	m.AddFunc("image/svg+xml", svg.Minify)
 	m.AddFuncRegexp(regexp.MustCompile("^(application|text)/(x-)?(java|ecma)script$"), js.Minify)
+	m.AddFuncRegexp(regexp.MustCompile("[/+]json$"), json.Minify)
+	m.AddFuncRegexp(regexp.MustCompile("[/+]xml$"), xml.Minify)
 
-	// entries, err := os.ReadDir("./sample")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// _ = make([]fs.FileInfo, 0, len(entries))
-	//
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// for _, file := range entries {
-	// 	if strings.Contains(file.Name(), ".js") ||
-	// 		strings.Contains(file.Name(), ".css") {
-	// 		fmt.Println(file.Name())
-	// 		if file.IsDir() {
-	// 			fmt.Println("dir")
-	// 		} else {
-	// 			fmt.Println("file")
-	// 		}
-	// 	}
-	// }
+	err := executeMinify(m, inputPath, outputPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	err := filepath.Walk("./sample", func(path string, info os.FileInfo, err error) error {
-		if strings.Contains(path, ".js") ||
-			strings.Contains(path, ".css") {
+	fmt.Println("- minify finish -")
+}
 
-			fmt.Println(path)
-			if info.IsDir() {
-				fmt.Println("dir")
-			} else {
-				fmt.Println("file")
-				// pathを読み込んでreader生成、minifyして、writerに書き込む
-				reader, err := os.Open(path)
-				if err != nil {
-					return err
-				}
-				defer func(reader *os.File) {
-					_ = reader.Close()
-				}(reader)
-				// writerの用意
-				err = os.MkdirAll("./dist/"+filepath.Dir(path), 0777)
-				if err != nil {
-					return err
-				}
-				writer, err := os.Create("./dist/" + path)
-				if err != nil {
-					return err
-				}
-				defer func(writer *os.File) {
-					_ = writer.Close()
-				}(writer)
+func executeMinify(m *minify.M, inputPath string, outputPath string) error {
+	fmt.Println("executeMinify")
+	var mediaType string
+	err := filepath.Walk(inputPath, func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(path, ".html") {
+			mediaType = "text/html"
+		} else if strings.Contains(path, ".js") {
+			mediaType = "text/javascript"
+		} else if strings.Contains(path, ".css") {
+			mediaType = "text/css"
+		} else if strings.Contains(path, ".json") {
+			mediaType = "application/json"
+		} else if strings.Contains(path, ".xml") {
+			mediaType = "application/xml"
+		} else if strings.Contains(path, ".svg") {
+			mediaType = "image/svg+xml"
+		} else {
+			mediaType = ""
+		}
 
-				err = m.Minify("text/css", writer, reader)
-				if err != nil {
-					return err
-				}
+		if mediaType == "" {
+			return nil
+		}
 
+		fmt.Println(path)
+		if !info.IsDir() {
+			// pathを読み込んでreader生成、minifyして、writerに書き込む
+			reader, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer func(reader *os.File) {
+				_ = reader.Close()
+			}(reader)
+
+			// writerの用意
+			err = os.MkdirAll(outputPath+"/"+filepath.Dir(path), 0777)
+			if err != nil {
+				return err
+			}
+			writer, err := os.Create(outputPath + "/" + path)
+			if err != nil {
+				return err
+			}
+			defer func(writer *os.File) {
+				_ = writer.Close()
+			}(writer)
+
+			err = m.Minify(mediaType, writer, reader)
+			if err != nil {
+				return err
 			}
 		}
 		return nil
 	})
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
+	return nil
 }
